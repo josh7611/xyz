5ec287c88938e9ca4faa59412a419ae58fab2630
Josh Hsu <josh.hsu@zepellin.com>
2021-09-03 07:35:42 +0800

    feat: Add wrapper between domain model and db entity because it is better to separate data model from different origins

--- app/src/main/java/com/xyz/app/data/db/entity/CurrencyEntity.kt
+++ app/src/main/java/com/xyz/app/data/db/entity/CurrencyEntity.kt
@@ -3,10 +3,19 @@ package com.xyz.app.data.db.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.xyz.app.domain.CurrencyInfo
 
 @Entity(tableName = "currency")
 data class CurrencyEntity(
     @PrimaryKey val id: String,
     @ColumnInfo(name = "name") val name: String,
-    @ColumnInfo(name = "symbol") val symbol: String,
-)
+    @ColumnInfo(name = "symbol") val symbol: String
+) {
+    fun asCurrencyInfo(): CurrencyInfo {
+        return CurrencyInfo(id, name, symbol)
+    }
+}
+
+fun CurrencyInfo.asEntity(): CurrencyEntity {
+    return CurrencyEntity(id, name, symbol)
+}
