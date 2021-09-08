d89cd220a598786f217f8ed2a40262d65186e480
Josh Hsu <josh.hsu@zepellin.com>
2021-09-09 07:21:10 +0800

    feat: Add button fetch and sort currency list

--- app/src/main/java/com/xyz/app/MainActivity.kt
+++ app/src/main/java/com/xyz/app/MainActivity.kt
@@ -1,18 +1,72 @@
 package com.xyz.app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.xyz.app.data.vo.Status
+import com.xyz.app.databinding.ActivityMainBinding
 import com.xyz.app.ui.main.CurrencyListFragment
+import com.xyz.app.ui.main.CurrencyListViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: CurrencyListViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+    private var toast: Toast? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, CurrencyListFragment.newInstance())
-                .commitNow()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, CurrencyListFragment.newInstance())
+            .commitNow()
+
+        initUi()
+        observeCurrencyInfo()
+    }
+
+    private fun initUi() {
+        binding.fetchCurrencyListButton.setOnClickListener {
+            viewModel.fetchCurrencyInfo()
+        }
+
+        binding.sortCurrencyListButton.setOnClickListener {
+            viewModel.sortCurrencyInfo()
         }
     }
+
+
+
+    private fun observeCurrencyInfo() {
+        viewModel.currencyInfos.observe(this) { resource ->
+            when (resource.status) {
+                Status.SUCCESS -> {
+                    (supportFragmentManager.findFragmentById(R.id.container) as? CurrencyListFragment)?.updateCurrencyList(
+                        resource.data ?: emptyList()
+                    )
+                }
+                Status.ERROR -> {
+                    toast?.cancel()
+                    toast = Toast.makeText(
+                        this,
+                        getString(
+                            resource.exception?.defaultDisplayedStringResId
+                                ?: R.string.global_error_unknown
+                        ),
+                        Toast.LENGTH_LONG
+                    ).also { it.show() }
+                }
+                Status.LOADING -> {
+
+                }
+            }
+        }
+
+    }
+
 }
--- app/src/main/java/com/xyz/app/ui/main/CurrencyListViewModel.kt
+++ app/src/main/java/com/xyz/app/ui/main/CurrencyListViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.xyz.app.data.ApplicationException.SortBeforeFetchException
 import com.xyz.app.data.repository.CurrencyInfoRepository
 import com.xyz.app.data.vo.Resource
 import com.xyz.app.domain.CurrencyInfo
@@ -16,6 +17,7 @@ private const val TAG = "CurrencyListViewModel"
 
 class CurrencyListViewModel : ViewModel() {
     private val currencyInfoRepository: CurrencyInfoRepository by SimpleDi
+    private lateinit var currencyList: List<CurrencyInfo>
 
     val currencyInfos: LiveData<Resource<List<CurrencyInfo>>>
         get() = _currencyInfos
@@ -27,11 +29,34 @@ class CurrencyListViewModel : ViewModel() {
             if (shouldLoadTestData()) {
                 currencyInfoRepository.update(CurrencyInfoFactory.list())
             }
-            _currencyInfos.value = Resource.success(currencyInfoRepository.list())
+            currencyList = currencyInfoRepository.list().also {
+                _currencyInfos.value = Resource.success(it)
+            }
         }
     }
 
     private suspend fun shouldLoadTestData() = withContext(Dispatchers.IO) {
         currencyInfoRepository.list().isEmpty()
+    }
+
+    fun sortCurrencyInfo() {
+        if (!::currencyList.isInitialized) {
+            _currencyInfos.value = Resource.error(
+                msg = SortBeforeFetchException.reason,
+                data = null,
+                exception = SortBeforeFetchException
+            )
+            return
+        }
+
+        _currencyInfos.value = Resource.loading(null)
+        viewModelScope.launch {
+            currencyList.sortedBy {
+                it.name
+            }.also {
+                _currencyInfos.value = Resource.success(it)
+            }
+
+        }
     }
 }
