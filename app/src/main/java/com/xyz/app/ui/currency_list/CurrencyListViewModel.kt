1d049d06f7df6855c4eb15de7bc6dbbdbfaf6605
Josh Hsu <josh.hsu@zepellin.com>
2021-09-10 08:23:18 +0800

    refactor: Move sort function to IO thread and make a defensive copy to sort

--- app/src/main/java/com/xyz/app/ui/currency_list/CurrencyListViewModel.kt
+++ app/src/main/java/com/xyz/app/ui/currency_list/CurrencyListViewModel.kt
@@ -9,15 +9,14 @@ import com.xyz.app.data.repository.CurrencyInfoRepository
 import com.xyz.app.data.vo.Resource
 import com.xyz.app.domain.CurrencyInfo
 import com.xyz.app.infrastructure.SimpleDi
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 private const val TAG = "CurrencyListViewModel"
 
 class CurrencyListViewModel : ViewModel() {
     private val currencyInfoRepository: CurrencyInfoRepository by SimpleDi
     private lateinit var currencyList: List<CurrencyInfo>
+    private var job: Job? = null
 
     val currencyInfos: LiveData<Resource<List<CurrencyInfo>>>
         get() = _currencyInfos
@@ -57,13 +56,27 @@ class CurrencyListViewModel : ViewModel() {
         }
 
         _currencyInfos.value = Resource.loading(null)
-        viewModelScope.launch {
-            currencyList.sortedBy {
-                it.name
-            }.also {
-                _currencyInfos.value = Resource.success(it)
-            }
 
+        job = viewModelScope.launch(Dispatchers.Main) {
+            val sorted = sortCurrencyList(currencyList)
+            _currencyInfos.value = Resource.success(sorted)
         }
+    }
+
+    private suspend fun sortCurrencyList(origin: List<CurrencyInfo>): List<CurrencyInfo> =
+        withContext(Dispatchers.IO) {
+            // Make a delay to simulate the io operation because we usually sort large data by the backend api
+            delay(1000)
+
+            // Make a copy in order not to break the order of the original list on other thread
+            val copy = origin.toMutableList()
+            copy.sortedBy {
+                it.name
+            }
+        }
+
+    override fun onCleared() {
+        job?.cancel()
+        super.onCleared()
     }
 }
